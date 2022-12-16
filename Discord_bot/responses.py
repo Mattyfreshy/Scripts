135b6a18f02ef895243be9ae66de7ff7d7d6e998
Matthew Zou <mzou1212@gmail.com>
2022-12-16 05:15:47 -0500

    a

--- Discord_bot/responses.py
+++ Discord_bot/responses.py
@@ -2,7 +2,7 @@ import random
 from enum import Enum
 
 # Enum to store information about every command
-class inputs(Enum):
+class Inputs(Enum):
     roll = "Roll a Dice"
     
 # Function to handle responses
@@ -13,6 +13,14 @@ def handle_responses(message) -> str:
         return 'Howdy!'
     
     if p_message == 'roll':
-        return str(random.randint(1,6))    
+        return str(random.randint(1,6))   
+    
+    if p_message == "help":
+        # generate list of commands w/ corrosponding information.
+        help_lst = ['Below is a list of commands: \n']
+        for i in Inputs:
+            help_lst.append(i.name + ': ' + i.value)
+            
+        return '\n'.join(help_lst)
     
     return 'Message not handled'
