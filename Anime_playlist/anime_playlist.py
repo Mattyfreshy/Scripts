0efd4ead1f924d5ffebe3715de337e618dbf0ced
Matthew Zou <mzou1212@gmail.com>
2022-12-16 03:10:56 -0500

    update

--- Anime_playlist/anime_playlist.py
+++ Anime_playlist/anime_playlist.py
@@ -1,16 +1,16 @@
 
 # Global Variables
-watched = 'anime_watched.txt'
-to_watch = 'anime_to_watch.txt'
+watched_file = 'anime_watched.txt'
+to_watch_file = 'anime_to_watch.txt'
 
-def sort_txt(file: str, mode: str):
+def sort_txt(file: str):
     # open file in mode
-    with open(file, mode) as f:
+    with open(file, 'r+') as f:
         # store sorted list in lst
         lst = []
         for line in sorted(f):
-            print(line, end='')
-            lst.append(line)
+            if not line.isspace():
+                lst.append(line)
         
         # clear txt file
         f.truncate(0)
@@ -18,11 +18,27 @@ def sort_txt(file: str, mode: str):
         
         # write updated sorted list to txt file
         for anime in lst:
-            f.write(anime)
-        
+            f.write(anime.capitalize())
+
+def search_txt(file: str, target: str) -> bool:
+    # open file in read mode
+    with open(file, 'r') as f:
+        for line in f:
+            if target.lower() == line.lower():
+                return True
+        return False
+    
+def add_to_watch(anime: str):
+    # open file in read mode
+    with open(to_watch_file, 'a+') as f:
+        f.write('\n' + anime.capitalize() + '\n')
+    
+    sort_txt(to_watch_file)
+            
         
 def main():
-    sort_txt(to_watch, 'r+')
+    #sort_txt(to_watch_file)
+    add_to_watch("test")
 
 if __name__ == "__main__":
     main()
