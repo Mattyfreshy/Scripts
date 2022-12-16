06a6b872470cbf731158708130b11447c92d9788
Matthew Zou <mzou1212@gmail.com>
2022-12-16 04:31:44 -0500

    update

--- Discord_bot/Fresh_bot.py
+++ Discord_bot/Fresh_bot.py
@@ -1,15 +1,33 @@
 import discord
 import os
+import responses
 
-client = discord.Client()
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_responses(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
 
-@client.event
-async def on_ready():
-    print('Oni Chan!')
-    
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        await message.channel.send('pending')
+def run_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
         
-client.run(os.getenv('DISCORD_TOKEN'))
+    @client.event
+    async def on_message(message):
+        if message.content.startswith('!test'):
+            await message.channel.send('pending')
+            
+    client.run(os.getenv('DISCORD_TOKEN'))
+    
+    
+def main():
+    run_discord_bot()
+    
+if __name__ == '__main__':
+    main()
