90df9d9499ab35fe87bc5de4207c967103540286
Matthew Zou <mzou1212@gmail.com>
2022-12-16 04:04:28 -0500

    update?

---
+++ Discord_bot/Fresh_bot.py
@@ -0,0 +1,15 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Oni Chan!')
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith('/animelist'):
+        print('pending')
+        
+client.run(os.getenv('DISCORD_TOKEN'))
