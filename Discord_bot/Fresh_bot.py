a3ca39fd981e29239953f5c894dea3c9020c1d09
Matthew Zou <mzou1212@gmail.com>
2022-12-16 04:10:46 -0500

    update?

--- Discord_bot/Fresh_bot.py
+++ Discord_bot/Fresh_bot.py
@@ -9,7 +9,7 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.content.startswith('/animelist'):
-        print('pending')
+    if message.content.startswith('!animelist'):
+        await message.channel.send('pending')
         
 client.run(os.getenv('DISCORD_TOKEN'))
